5f0089a5e75a8f588af1150d48a1cc501253d051
lefthandeeeeeeed <39003945+lefthandeeeeeeed@users.noreply.github.com>
2018-05-08 14:48:01 +0900

    Update main.js

--- main.js
+++ main.js
@@ -78,8 +78,7 @@ var drawGraphBar = function(dataBar){
     type: 'bar', // グラフの種類（棒グラフを指定）
     data: { labels: label, datasets: [data1, data2]},
     options: option,
-    plugins: [dataLabelPlugin]
-  }
+   }
   var myChart = new Chart(ctx, config);
 
 };
@@ -152,35 +151,3 @@ window.onload=function () {
 
 };
 
-// Define a plugin to provide data labels
-　Chart.plugins.register({
-			afterDatasetsDraw: function(chart) {
-				var ctx = chart.ctx;
-
-				chart.data.datasets.forEach(function(dataset, i) {
-					var meta = chart.getDatasetMeta(i);
-					if (!meta.hidden) {
-						meta.data.forEach(function(element, index) {
-							// Draw the text in black, with the specified font
-							ctx.fillStyle = 'rgb(0, 0, 0)';
-
-							var fontSize = 16;
-							var fontStyle = 'normal';
-							var fontFamily = 'Helvetica Neue';
-							ctx.font = Chart.helpers.fontString(fontSize, fontStyle, fontFamily);
-
-							// Just naively convert to string for now
-							var dataString = dataset.data[index].toString();
-
-							// Make sure alignment settings are correct
-							ctx.textAlign = 'center';
-							ctx.textBaseline = 'middle';
-
-							var padding = 5;
-							var position = element.tooltipPosition();
-							ctx.fillText(dataString, position.x, position.y - (fontSize / 2) - padding);
-						});
-					}
-				});
-			}
-		});
